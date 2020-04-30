dc25807c978a60d55fefde80a63f1dc22adb6d85
Filippos Kontoes <fil13kon@gmail.com>
2020-04-30 16:56:02 +0300

    Updates

---
+++ precache-manifest.318f91e50bfe4f00f7738dc34e9e0f88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae45810e1eb9e0b06a08a7854f0fada",
+    "url": "/IdeasIntoCoading_ReactVersion/index.html"
+  },
+  {
+    "revision": "2a64e1773bd10caac565",
+    "url": "/IdeasIntoCoading_ReactVersion/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fc64d609eda827ba4c1d",
+    "url": "/IdeasIntoCoading_ReactVersion/static/css/main.de2d0af7.chunk.css"
+  },
+  {
+    "revision": "2a64e1773bd10caac565",
+    "url": "/IdeasIntoCoading_ReactVersion/static/js/2.d384a587.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/IdeasIntoCoading_ReactVersion/static/js/2.d384a587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc64d609eda827ba4c1d",
+    "url": "/IdeasIntoCoading_ReactVersion/static/js/main.edc04299.chunk.js"
+  },
+  {
+    "revision": "12f9ab6be3de909bf589",
+    "url": "/IdeasIntoCoading_ReactVersion/static/js/runtime-main.714c62ad.js"
+  },
+  {
+    "revision": "4fbc9e4e12b90a0e02195898907efe52",
+    "url": "/IdeasIntoCoading_ReactVersion/static/media/1075605a-6f18-4f8c-bdc0-ff6620554bb1DeathNote.4fbc9e4e.png"
+  },
+  {
+    "revision": "1499e33db7156dc92450403a89c20956",
+    "url": "/IdeasIntoCoading_ReactVersion/static/media/homepage.1499e33d.jpg"
+  },
+  {
+    "revision": "afdaf12055e92968a3230fbe08a11ef5",
+    "url": "/IdeasIntoCoading_ReactVersion/static/media/homepagev2.afdaf120.png"
+  }
+]);
